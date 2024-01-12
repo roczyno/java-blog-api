2ff1a20f520d0b8520247faa2f5ab23989f0a3d0
roczyno <adiabaroczyno9@gmail.com>
2024-01-12 13:28:31 +0000

    created post entity

---
+++ src/main/java/com/roczyno/blog/api/entity/Post.java
@@ -0,0 +1,24 @@
+package com.roczyno.blog.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="title",nullable = false)
+    private String title;
+    @Column(name="description",nullable = false)
+    private String description;
+    @Column(name="content",nullable = false)
+    private String content;
+}
